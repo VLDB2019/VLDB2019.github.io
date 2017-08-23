14a78647b701973736b5af43725e7e7dcd216d3e
Miki <mehran@barahmand.com>
2017-08-23 10:06:38 -0700

    Add coming soon and officers

---
+++ js/router.js
@@ -0,0 +1,41 @@
+(function(win, doc) {
+    var xhr;
+
+    win.loadPage = function(page) {
+        if (xhr) {
+            xhr.abort();
+        }
+
+        var mainBody = doc.querySelector('main.body');
+        if (!mainBody) return;
+
+        mainBody.classList.add('loading');
+
+        var headerCarousel = doc.querySelector('header .carousel');
+        if (headerCarousel) headerCarousel.classList.add('small');
+
+        xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', function() {
+            var content = this.responseText,
+                isNotFound = content.indexOf('data-page="404"') !== -1;
+
+            if (isNotFound) {
+                if (page !== 'coming-soon') loadPage('coming-soon');
+                return;
+            }
+
+            /*
+            [].forEach.call(mainBody.querySelectorAll('>*:not(.spinner)'), function(el) {
+                console.log('removed', el);
+                mainBody.removeChild(el);
+            });
+            */
+
+            mainBody.innerHTML = content + '<div class="spinner"></div>';
+
+            mainBody.classList.remove('loading');
+        });
+        xhr.open('GET', '/pages/' + page + '.html');
+        xhr.send();
+    };
+})(window, document);
---
+++ pages/coming-soon.html
@@ -0,0 +1,3 @@
+<section class="coming-soon">
+    <h2>Coming soon</h2>
+</section>
